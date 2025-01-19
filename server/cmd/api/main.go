4c122f090eb10faea2a1f54768707fa28c2042a6
Niraj Dhakal <yboii34@gmail.com>
2025-01-19 17:13:00 -0500

    go scripts updated

--- server/cmd/api/main.go
+++ server/cmd/api/main.go
@@ -12,7 +12,7 @@ func main() {
 	// Set up HTTP routes
 	http.HandleFunc("/trigger", handlers.TriggerPythonScript) // Trigger Python script
 	http.HandleFunc("/health", handlers.HealthCheck)          // Health check
-
+	http.HandleFunc("/followers", handlers.TriggerFollowersScript)
 	// Start the server
 	port := ":8080"
 	fmt.Printf("✅ API server is running on http://localhost%s\n", port)
